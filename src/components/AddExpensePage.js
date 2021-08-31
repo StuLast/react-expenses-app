a190b7d7298a7beabf7d3f2d2b2027d3a61a1d55
Stu Last <stuart@theaudioprogrammer.com>
2021-08-31 19:58:23 +0100

    Add in expense form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,12 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
-const AddExpensePage = () => <div>This is from my Add Expense Component</div>;
+const AddExpensePage = () => (
+  <div>
+    <h1> Add Expense </h1>
+    <ExpenseForm />
+  </div>
+);
+
 
 export default AddExpensePage;
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFilters";
+
 const ExpenseDashboardPage = () => (
-  <div>This is from my Dashboard Component</div>
+  <div>
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
 );
 
 export default ExpenseDashboardPage;
