454d1ad69c3158938a5a0a16f4f435460ac64409
Stu Last <stuart@theaudioprogrammer.com>
2021-09-17 15:23:36 +0100

    Implement credentials and env security

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {
-  getDatabase,
-  ref,
-  set,
-  remove,
-  update,
-  get,
-  onValue,
-  off, 
-  push,
-  onChildRemoved,
-  onChildChanged,
-  onChildAdded
-} from "firebase/database";
+import { getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoFT-erWMmwmeLmmUJ382hrYFzQ_WxG7o",
-  authDomain: "react-expenses-demo-app-134ea.firebaseapp.com",
-  projectId: "react-expenses-demo-app-134ea",
-  storageBucket: "react-expenses-demo-app-134ea.appspot.com",
-  messagingSenderId: "713249197961",
-  appId: "1:713249197961:web:b4376033be919b9a905b58",
-  measurementId: "G-4X4554DG03",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import dotenv from 'dotenv'
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
+
+dotenv.config({ path: '.env.test'});
 
