4c915416f4b10bff092b91ed6913fba1e982edea
Stu Last <stuart@spycedconcepts.uk>
2021-09-19 15:35:19 +0100

    Setup footer component and style file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,30 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => (
   <footer className="footer">
-    <Link to="about">About this demo</Link>
+    <div className="footer__top">
+      <div className="footer__column">
+        <h3>Links:</h3>
+        <Link to="/about">About this demo</Link>
+      </div>
+      <div className="footer__column"></div>
+      <div className="footer__column">
+        <h3>Privacy:</h3>
+        <Link to="/privacy">Privacy</Link>
+        <Link to="/cookies">Cookies</Link>
+      </div>
+    </div>
+    <div className="footer__bottom">
+      <div>
+        Powered by:
+        <Link to="https://spycedconcepts.uk" target="_blank">
+          Spyced Concepts
+        </Link>
+        . Programmed by:
+        <Link href="https://stulast.co.uk" target="_blank">
+          Stu Last
+        </Link>
+      </div>
+    </div>
   </footer>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,6 +7,9 @@ import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import NotFoundPage from "../components/NotFoundPage";
+import AboutPage from "../components/AboutPage";
+import PrivacyPage from "../components/PrivacyPage";
+import CookiesPage from "../components/CookiesPage";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 
@@ -20,6 +23,9 @@ const AppRouter = () => (
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <PublicRoute path="/about" component={AboutPage} />
+        <PublicRoute path="/privacy" component={PrivacyPage} />
+        <PublicRoute path="/cookies" component={CookiesPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
